d7deefcb9bdaf84aa971ca19e91f7c0b63597e1c
John Graham <johnegraham2@gmail.com>
2021-02-14 12:06:44 -0500

    weekly allowance

--- client/src/App.js
+++ client/src/App.js
@@ -4,92 +4,91 @@ import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
-  return (
-    <Container>
-      <Menu  size="massive" color="teal">
-        <Menu.Item name="Accounts" active />
-      </Menu>
-      
-      <div className="account-group">
-        <Grid>
-          <Grid.Row>
-            <Grid.Column>
-              <Header as="h2">Jack</Header>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row columns={3}>
-            <Grid.Column>
-              <Card>
-                <Card.Content>
-                  <Card.Header>Jack Spending</Card.Header>
-                  <Header as="h2">$1.18</Header>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Card>
-                <Card.Content>
-                  <Card.Header>Jack Saving</Card.Header>
-                  <Header as="h2">$24.00</Header>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Card>
-                <Card.Content>
-                  <Card.Header>Jack Giving</Card.Header>
-                  <Header as="h2">$24.00</Header>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
-
-      <div className="account-group">
-        <Grid>
-          <Grid.Row>
-            <Grid.Column>
-              <Header as="h2">Ellen</Header>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row columns={3}>
-            <Grid.Column>
-              <Card>
-                <Card.Content>
-                  <Card.Header>Ellen Spending</Card.Header>
-                  <Header as="h2">$24.00</Header>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Card>
-                <Card.Content>
-                  <Card.Header>Ellen Saving</Card.Header>
-                  <Header as="h2">$24.00</Header>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Card>
-                <Card.Content>
-                  <Card.Header>Ellen Giving</Card.Header>
-                  <Header as="h2">$24.00</Header>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
-    </Container>
+    return (
+        <Container>
+            <Menu  size="massive" color="teal">
+                <Menu.Item name="Accounts" active />
+            </Menu>
     
-  );
+            <div className="account-group">
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Header as="h2">Jack</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row columns={3}>
+                        <Grid.Column>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>Jack Spending</Card.Header>
+                                    <Header as="h2">$4.68</Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>Jack Saving</Card.Header>
+                                    <Header as="h2">$27.50</Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>Jack Giving</Card.Header>
+                                    <Header as="h2">$27.50</Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+
+            <div className="account-group">
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Header as="h2">Ellen</Header>
+                        </Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row columns={3}>
+                        <Grid.Column>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>Ellen Spending</Card.Header>
+                                    <Header as="h2">$27.50</Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>Ellen Saving</Card.Header>
+                                    <Header as="h2">$27.50</Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+
+                        <Grid.Column>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>Ellen Giving</Card.Header>
+                                    <Header as="h2">$27.50</Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        </Container>
+    );
 }
 
 export default App;
