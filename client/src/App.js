f93a9bf2de9f3ea38d4cf8e2476aea9ee8b897f8
John Graham <johnegraham2@gmail.com>
2021-04-04 18:39:23 -0400

    jack's easter money

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
                             <Card>
                                 <Card.Content>
                                     <Card.Header>Jack Spending</Card.Header>
-                                    <Header as="h2">$3.65</Header>
+                                    <Header as="h2">$11.65</Header>
                                 </Card.Content>
                             </Card>
                         </Grid.Column>
